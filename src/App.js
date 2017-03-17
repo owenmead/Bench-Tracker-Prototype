02590730a04956580000a23c7a02fd024532c37a
Owen Mead-Robins <owenmead@gmail.com>
2017-03-17 08:49:51 -0700

    Init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Tracker
+      </div>
+    );
+  }
+}
+
+export default App;
